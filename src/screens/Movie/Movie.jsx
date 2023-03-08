e6b650f9e4d625d67104270efe68c6b41ab51386
bombasters <59049797+bombasters@users.noreply.github.com>
2023-03-08 19:04:47 +0300

    Add files via upload

--- src/screens/Movie/Movie.jsx
+++ src/screens/Movie/Movie.jsx
@@ -1,10 +1,22 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import './Movie.scss'
+import Rate from '../../UI/Rate'
 
 const Movie = () => {
 	const [movie, setMovie] = useState(null)
-
+	const [staffs, setStaffs] = useState([])
+	const getRate = rating => {
+		if (rating > 7) {
+			return 'good_rate'
+		}
+		if (rating > 4) {
+			return 'normal_rate'
+		} else {
+			return 'bad_rate'
+		}
+	}
 	const { id } = useParams()
 	useEffect(() => {
 		axios
@@ -12,9 +24,62 @@ const Movie = () => {
 				headers: { 'X-API-KEY': '21a4979f-0100-4037-9d9b-8a2204f81150' },
 			})
 			.then(res => setMovie(res.data))
+		axios
+			.get(`https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=${id}`, {
+				headers: { 'X-API-KEY': '21a4979f-0100-4037-9d9b-8a2204f81150' },
+			})
+			.then(res => setStaffs(res.data))
 	}, [])
+	console.log(movie)
+	console.log(staffs)
 	return (
-		<div>{movie ? <div>Movie {movie.nameRu}</div> : <h1>Movie {id}</h1>}</div>
+		<div>
+			{movie ? (
+				<>
+					<div className='flex flex-col gap-5 pb-10'>
+						<img
+							className='mobile:w-full desktop:w-[400px]'
+							src={movie.posterUrl}
+							alt=''
+						/>
+						<div className='w-full px-3 flex items-center justify-between'>
+							<span className='font-bold'>{movie.nameRu}</span>
+							<div
+								className={`${getRate(
+									movie.ratingKinopoisk
+								)} flex items-center gap-2`}>
+								<Rate />
+								{movie.ratingKinopoisk && <span>{movie.ratingKinopoisk}</span>}
+							</div>
+						</div>
+						<p className='px-3 text-xs opacity-60'>{movie.description}</p>
+						<h1 className='px-3 font-bold mb-[10px]'>Актерский состав</h1>
+						<div className='px-3 w-full desktop:hidden whitespace-nowrap overflow-x-scroll overflow-y-hidden'>
+							{staffs &&
+								staffs.map(staff => {
+									return (
+										<div className='inline-block'>
+											<img
+												className='rounded-lg w-[160px] h-[250px] object-fill mr-[30px] mb-[15px]'
+												src={staff.posterUrl}
+												alt=''
+											/>
+											<div className='flex flex-col gap-2'>
+												<span className='font-medium'>{staff.nameRu}</span>
+												<span className='text-xs opacity-60'>
+													{staff.professionText}
+												</span>
+											</div>
+										</div>
+									)
+								})}
+						</div>
+					</div>
+				</>
+			) : (
+				<h1>Запрос к Кинопоиску...</h1>
+			)}
+		</div>
 	)
 }
 
